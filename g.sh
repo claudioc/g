fe4e4eff99993cb46c55cba9fa80b322aa1b9379
claudioc <claudio.cicali@n26.com>
2019-02-06 16:04:21 +0100

    Refactoring

--- g.sh
+++ g.sh
@@ -24,9 +24,17 @@ G_C_NEEDS_TICKET=${G_C_NEEDS_TICKET:-0}
 G_C_IS_SMART=${G_C_IS_SMART:-1}
 G_REMOTE=${G_REMOTE:-origin}
 
+argc=${#}
+
+main() {
+  validate_environment
+  execute_command ${1} ${2-} ${3-}
+}
+
 ghelp () {
   cat <<EOT
-a    : git add -u
+a    : git add -u (with a second parameter also commits)
+A \$1: git add \$1
 c \$1: git commit -m \$1, with ticket number detection
 d    : git diff + git diff --staged
 D \$1: Shows the differences between HEAD and another branch
@@ -56,6 +64,13 @@ assert_one_param () {
   fi
 }
 
+assert_one_or_zero_params () {
+  if [[ ${argc} -gt 2 ]]; then
+    echo "🚫  This command needs one or zero parameters only"
+    ghelp
+  fi
+}
+
 assert_not_dirty () {
   local git_dirty=$(git diff-index --name-only HEAD --)
 
@@ -65,168 +80,223 @@ assert_not_dirty () {
   fi
 }
 
-argc=${#}
-if [[ ${argc} -eq 0 ]]; then
-  ghelp
-fi
+validate_environment() {
+  if [[ ${argc} -eq 0 ]]; then
+    ghelp
+  fi
 
-cmd=${1}
+  if [[ -z "$(git rev-parse --git-dir 2> /dev/null)" ]]; then
+    echo "😊  I work better when run from inside a git repository."
+    exit 1
+  fi
 
-if [[ -z "$(git rev-parse --git-dir 2> /dev/null)" ]]; then
-  echo "😊  I work better when run from inside a git repository."
-  exit 1
-fi
-
-{
-  git_branch=$(git symbolic-ref HEAD | sed 's/refs\/heads\///')
-} || {
-  echo "😬  Sorry, operation not supported when in detached state."
-  exit 1
+  {
+    git_branch=$(git symbolic-ref HEAD | sed 's/refs\/heads\///')
+  } || {
+    echo "😬  Sorry, operation not supported when in detached state."
+    exit 1
+  }
 }
 
-case ${cmd} in
-  # Git add -u
-  a)
-    assert_no_params
-    git add -u
-    # Check if we have untracked changes too
-    untracked=$(git ls-files --others --exclude-standard)
-    if [[ -n "${untracked}" ]]; then
-      echo "⚠️  Done, but you may have untracked files to add too (use \`g s\` to see them)."
-    fi
-    ;;
+execute_command() {
+  case ${1} in
+    a) # Git add -u
+      assert_one_or_zero_params
+      command_a ${2-}
+      ;;
 
-  # Git 'smart' commit
-  c)
-    assert_one_param
-    if [[ "${G_C_IS_SMART}" != "1" ]]; then
-      git commit -m "${2}"
-    else
-      # Extracts and use a ticket id from a branch name in one of these forms:
-      # 'claudioc/IT-123_something_something'
-      # 'IT-123_something_something'
-      # 'something_something_IT-123'
-      # 'something_something' (produces '')
-      # It will fail with 'something_something_ANDMOREIT-123'
-      regexp="(${G_C_TICKET_REGEXP})"
-      branch_id=${G_C_DEFAULT_TICKET}
-      if [[ ${git_branch} =~ ${regexp} ]]; then
-        # Bash doesn't support non-greedy RE, so we need to remove the final part of the match
-        branch_id=${BASH_REMATCH[1]//_*}
-      else
-        if [[ "${G_C_NEEDS_TICKET}" != "0" ]]; then
-          echo "🔍 A ticket number was not found analyzing the branch name."
-          exit 1
-        fi
-      fi
-      if [[ "${branch_id}" != "" ]]; then
-        git commit -m "[${branch_id}] ${2}"
-      else
-        git commit -m "${2}"
-      fi
-    fi
-    ;;
-
-  # Git diff (warning: it also looks into already staged files)
-  d)
-    assert_no_params
-    git diff
-    git diff --staged
-    ;;
-
-  # Git difference between HEAD and another branch
-  # (L is maintained for backward compatibility)
-  D|L)
-    assert_one_param
-    git log --oneline HEAD...${G_REMOTE}/${2}
-    ;;
-
-  h)
-    ghelp
-    ;;
-
-  # Checking out master
-  m)
-    assert_no_params
-    if [[ ${git_branch} != "master" ]]; then
-      git checkout master
-      git pull
-    else
-      echo "⚠️  You already are in master; just pulling"
-      git pull
-    fi
-    ;;
-
-  # Git checkout a local branch (or creates it)
-  g)
-    if [[ ${argc} -eq 1 ]]; then
-      git for-each-ref --sort='-committerdate' --format='%(refname)%09%(committerdate)' refs/heads | head -n 15 | sed -e 's-refs/heads/--'
-    else
+    A) # Git add
       assert_one_param
-      assert_not_dirty
-      branch_name=${2}
-      if [[ "${git_branch}" == "${branch_name}" ]]; then
-        echo "🤔  You already are in ${branch_name}"
+      command_A ${2}
+      ;;
+
+    c) # Git 'smart' commit
+      assert_one_param
+      command_c ${2}
+      ;;
+
+    # Git diff (warning: it also looks into already staged files)
+    d)
+      assert_no_params
+      command_d
+      ;;
+
+    # Git difference between HEAD and another branch
+    # (L is maintained for backward compatibility)
+    D|L)
+      assert_one_param
+      command_D ${2}
+      ;;
+
+    h)
+      ghelp
+      ;;
+
+    # Git checkout a local branch (or creates it)
+    g)
+      assert_one_or_zero_params
+      command_g ${2-}
+      ;;
+
+    # Interactively changes branch matching $1
+    G)
+      assert_no_params
+      command_G
+      ;;
+
+    # Checking out master
+    m)
+      assert_no_params
+      command_m
+      ;;
+
+    # Git pull
+    p)
+      assert_no_params
+      command_p
+      ;;
+
+    # Git push
+    P)
+      assert_no_params
+      command_P
+      ;;
+
+    # Git status
+    s)
+      assert_no_params
+      command_s
+      ;;
+
+    # Pass-through any command to git
+    *)
+      git ${*}
+      ;;
+  esac
+}
+
+command_a() {
+  git add -u
+  # Check if we have untracked changes too
+  untracked=$(git ls-files --others --exclude-standard)
+  if [[ -n "${untracked}" ]]; then
+    echo "⚠️  Done, but you may have untracked files to add too (use \`g s\` to see them)."
+  fi
+
+  if [[ "${1-}" != "" ]]; then
+    command_c ${1}
+  fi
+}
+
+command_A() {
+  git add ${1}
+}
+
+command_c() {
+  if [[ "${G_C_IS_SMART}" != "1" ]]; then
+    git commit -m "${1}"
+  else
+    # Extracts and use a ticket id from a branch name in one of these forms:
+    # 'claudioc/IT-123_something_something'
+    # 'IT-123_something_something'
+    # 'something_something_IT-123'
+    # 'something_something' (produces '')
+    # It will fail with 'something_something_ANDMOREIT-123'
+    regexp="(${G_C_TICKET_REGEXP})"
+    branch_id=${G_C_DEFAULT_TICKET}
+    if [[ ${git_branch} =~ ${regexp} ]]; then
+      # Bash doesn't support non-greedy RE, so we need to remove the final part of the match
+      branch_id=${BASH_REMATCH[1]//_*}
+    else
+      if [[ "${G_C_NEEDS_TICKET}" != "0" ]]; then
+        echo "🔍  A ticket number was not found analyzing the branch name."
+        exit 1
+      fi
+    fi
+    if [[ "${branch_id}" != "" ]]; then
+      git commit -m "[${branch_id}] ${1}"
+    else
+      git commit -m "${1}"
+    fi
+  fi
+}
+
+command_d() {
+  git diff
+  git diff --staged
+}
+
+command_D() {
+  git log --oneline HEAD...${G_REMOTE}/${1}
+}
+
+command_g() {
+  if [[ "${1-}" == "" ]]; then
+    git for-each-ref --sort='-committerdate' --format='%(refname)%09%(committerdate)' refs/heads | head -n 15 | sed -e 's-refs/heads/--'
+  else
+    assert_not_dirty
+    branch_name=${1}
+    if [[ "${git_branch}" == "${branch_name}" ]]; then
+      echo "🤔  You already are in ${branch_name}"
+    else
+      if [[ -n $(git rev-parse --verify --quiet ${branch_name}) ]]; then
+        git checkout ${1}
       else
-        if [[ -n $(git rev-parse --verify --quiet ${branch_name}) ]]; then
-          git checkout ${2}
-        else
-          read -r -n 1 -p "❓ Do you want to create the branch \"${branch_name}\" (y/N)? "
-          if [[ "${REPLY}" == "y" ]]; then
-            echo
-            git checkout -b ${branch_name}
-          fi
+        read -r -n 1 -p "❓ Do you want to create the branch \"${branch_name}\" (y/N)? "
+        if [[ "${REPLY}" == "y" ]]; then
+          echo
+          git checkout -b ${branch_name}
         fi
       fi
     fi
-    ;;
+  fi
+}
 
-  # Interactively changes branch matching $1
-  G)
-    assert_not_dirty
-    branch_name=${2-''}
+command_G() {
+  assert_not_dirty
+  branch_name=${1-''}
 
-    if [[ -z ${branch_name} ]]; then
-      branches=$(git for-each-ref --sort='-committerdate' --format='%(refname)' refs/heads --count 15 | sed -e 's-refs/heads/--')
-    else
-      branches=$(git for-each-ref --sort='-committerdate' --format='%(refname)' refs/heads refs/remotes | grep "${branch_name}" | sed "s/refs\/remotes\/${G_REMOTE}\///" | sed 's/refs\/heads\///' | uniq || true)
+  if [[ -z ${branch_name} ]]; then
+    branches=$(git for-each-ref --sort='-committerdate' --format='%(refname)' refs/heads --count 15 | sed -e 's-refs/heads/--')
+  else
+    branches=$(git for-each-ref --sort='-committerdate' --format='%(refname)' refs/heads refs/remotes | grep "${branch_name}" | sed "s/refs\/remotes\/${G_REMOTE}\///" | sed 's/refs\/heads\///' | uniq || true)
+  fi
+
+  if [[ -z "${branches}" ]]; then
+    echo "🔍  No branches found matching '${branch_name}'"
+    exit 0
+  fi
+
+  PS3="Select a branch (or ^C): "
+  declare -a branches=(${branches})
+  select opt in "${branches[@]}"; do
+    if [[ "${opt}" != "" ]]; then
+      git checkout ${opt}
     fi
+    break
+  done
+}
 
-    if [[ -z "${branches}" ]]; then
-      echo "🔍  No branches found matching '${branch_name}'"
-      exit 0
-    fi
-
-    PS3="Select a branch (or ^C): "
-    declare -a branches=(${branches})
-    select opt in "${branches[@]}"; do
-      if [[ "${opt}" != "" ]]; then
-        git checkout ${opt}
-      fi
-      break
-    done
-    ;;
-
-  # Git pull
-  p)
-    assert_no_params
+command_m() {
+  if [[ ${git_branch} != "master" ]]; then
+    git checkout master
     git pull
-    ;;
+  else
+    echo "⚠️  You already are in master; just pulling"
+    git pull
+  fi
+}
 
-  # Git push
-  P)
-    assert_no_params
-    git push -u ${G_REMOTE} ${git_branch}
-    ;;
+command_p() {
+  git pull
+}
 
-  # Git status
-  s)
-    assert_no_params
-    git status -s
-    ;;
+command_P() {
+  git push -u ${G_REMOTE} ${git_branch}
+}
 
-  # Pass-through any command to git
-  *)
-    git ${*}
-    ;;
-esac
+command_s() {
+  git status -s
+}
+
+main ${1} ${2-} ${3-}
